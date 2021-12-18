fdde2b4a58df680384ca17ed2dfcb9569a98400b
H4R5H1T-007 <hgupta1153@gmail.com>
2021-12-18 16:47:48 +0530

    Created script for fetching from api
    In this commit I have added python script to fetch data from youtube api

---
+++ main/fetch.py
@@ -0,0 +1,51 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import json
+import os
+
+with open(os.path.join('/home/harshit/Desktop/Django/fampay', 'cred.json')) as cred:
+    key = json.load(cred)
+
+DEVELOPER_KEY = key['API_KEY']
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
+
+required = [
+    "title",
+    "description",
+    "publishedAt",
+]
+
+def youtube_search(options):
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+        developerKey=DEVELOPER_KEY)
+
+    search_response = youtube.search().list(
+        q=options['query'],
+        part='id,snippet',
+        maxResults=options['max_result'],
+        order='date',
+    ).execute()
+
+    videos = dict()
+
+    for search_result in search_response.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            temp = dict()
+            for req in required:
+                temp[req] = search_result['snippet'][req]
+            temp['thumbnail'] = search_result['snippet']['thumbnails']['high']['url']
+            temp["video_url"] = "".join(['https://www.youtube.com/watch?v=', search_result['id']['videoId']])
+            videos[search_result['id']['videoId']] = temp
+    
+    return videos
+
+if __name__ == '__main__':
+    args = {
+        "query" : "Google",
+        "max_result": 25,
+    }
+    try:
+        youtube_search(args)
+    except HttpError as e:
+        print ('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)) 
