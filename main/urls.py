8c07adc4a05125aa3d1fa47047d48c86c4e7d135
H4R5H1T-007 <hgupta1153@gmail.com>
2021-12-19 17:29:50 +0530

    Created Async data fetching method.
    To call fetching, I have created a url route which will trigger
    python threading module and run the fetching method indefinitely.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import home
+from .views import *
 
 urlpatterns = [
+    path("fetch-api-from-youtube/", fetch, name='fetch-data'),
     path("", home, name='home-page'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,16 @@
 # from django.http import request
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+import threading
+import time
 from .fetch import youtube_search
 from .models import Youtube_data
+from asgiref.sync import sync_to_async
 
-def home(request):
+# @sync_to_async(thread_sensitive=False)
+def fetch_data():
     temp = youtube_search({
-        "query":"Google",
-        "max_result":25,
+        'query':'code',
+        'max_result':25,
     })
     for keys, items in temp.items():
         if(not Youtube_data.objects.filter(id=keys).exists()):
@@ -19,6 +23,19 @@ def home(request):
                 video_url = items['video_url'],
             )
             mod.save()
+
+def run_func(func):
+    while(True):
+        func()
+        time.sleep(10)
+
+def fetch(request):
+    t = threading.Thread(target=run_func, kwargs={'func':fetch_data}, daemon=True)
+    t.start()
+    return redirect('home-page')
+
+def home(request):
+    # fetch()
     context = {
         "posts" : Youtube_data.objects.all(),
     }
